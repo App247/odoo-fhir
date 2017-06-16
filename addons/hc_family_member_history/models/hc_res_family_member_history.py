d3ec57591681b69d6d15d960a9fb5d5b22a1dbd7
Luigi Sison <lsison@moxylus.com>
2017-06-15 21:12:08 -0700

    changes

--- addons/hc_family_member_history/models/hc_res_family_member_history.py
+++ addons/hc_family_member_history/models/hc_res_family_member_history.py
@@ -35,6 +35,11 @@ class FamilyMemberHistory(models.Model):
             ("health-unknown", "Health-Unknown")],
         default="partial", 
         help="A code specifying the status of the record of the family history of a specific family member.")
+    status_history_ids = fields.One2many(
+        comodel_name="hc.family.member.history.status.history", 
+        inverse_name="family_member_history_id", 
+        string="Status History", 
+        help="The status of the family member history over time.")
     data_absent_reason = fields.Selection(
         string="Data Absent Reason", 
         selection=[
@@ -331,8 +336,55 @@ class FamilyMemberHistory(models.Model):
 # fhs-1: Can have age[x] or born[x], but not both (expression : age.empty() or born.empty())
 # fhs-2: Can only have estimatedAge if age[x] is present (expression : age.exists() or estimatedAge.empty())
 
-
-
+    @api.model                          
+    def create(self, vals):                         
+        status_history_obj = self.env['hc.family.member.history.status.history']                        
+        res = super(FamilyMemberHistory, self).create(vals)                     
+        if vals and vals.get('status'):                     
+            status_history_vals = {                 
+                'family_member_history_id': res.id,             
+                'status': res.status,             
+                'start_date': datetime.today()              
+                }               
+            if vals.get('status') == 'entered-in-error':                    
+                status_history_vals.update({'end_date': datetime.today()})              
+            status_history_obj.create(status_history_vals)                  
+        return res                      
+                                
+    @api.multi                          
+    def write(self, vals):                          
+        status_history_obj = self.env['hc.family.member.history.status.history']                        
+        res = super(FamilyMemberHistory, self).write(vals)                      
+        status_history_record_ids = status_history_obj.search([('end_date','=', False)])                        
+        if status_history_record_ids:                       
+            if vals.get('status') and status_history_record_ids[0].status != vals.get('status'):                   
+                for status_history in status_history_record_ids:                
+                    status_history.end_date = datetime.strftime(datetime.today(), DTF)          
+                    time_diff = datetime.today() - datetime.strptime(status_history.start_date, DTF)            
+                    if time_diff:           
+                        days = str(time_diff).split(',')        
+                        if days and len(days) > 1:      
+                            status_history.time_diff_day = str(days[0]) 
+                            times = str(days[1]).split(':') 
+                            if times and times > 1: 
+                                status_history.time_diff_hour = str(times[0])
+                                status_history.time_diff_min = str(times[1])
+                                status_history.time_diff_sec = str(times[2])
+                        else:       
+                            times = str(time_diff).split(':')   
+                            if times and times > 1: 
+                                status_history.time_diff_hour = str(times[0])
+                                status_history.time_diff_min = str(times[1])
+                                status_history.time_diff_sec = str(times[2])
+                status_history_vals = {             
+                    'family_member_history_id': self.id,            
+                    'status': vals.get('status'),           
+                    'start_date': datetime.today()          
+                    }           
+                if vals.get('status') == 'entered-in-error':                
+                    status_history_vals.update({'end_date': datetime.today()})          
+                status_history_obj.create(status_history_vals)              
+        return res                      
 
 class FamilyMemberHistoryCondition(models.Model):   
     _name = "hc.family.member.history.condition"    
@@ -530,6 +582,36 @@ class FamilyMemberHistoryDefinition(models.Model):
                 hc_family_member_history_definition.definition_name = hc_family_member_history_definition.definition_plan_definition_id.name
             elif hc_family_member_history_definition.definition_type == 'questionnaire':   
                 hc_family_member_history_definition.definition_name = hc_family_member_history_definition.definition_questionnaire_id.name
+
+class FamilyMemberHistoryStatusHistory(models.Model):       
+    _name = "hc.family.member.history.status.history"   
+    _description = "Family Member History Status History"   
+        
+    family_member_history_id = fields.Many2one(
+        comodel_name="hc.res.family.member.history",
+        string="Family Member History",
+        help="Family Member History associated with this Family Member History Status History.")
+    status = fields.Char(   
+        string="Status",
+        help="The status of the family member history.")
+    start_date = fields.Datetime(   
+        string="Start Date",
+        help="Start of the period during which this family member history status is valid.")
+    end_date = fields.Datetime( 
+        string="End Date",
+        help="End of the period during which this family member history status is valid.")
+    time_diff_day = fields.Char(    
+        string="Time Diff (days)",
+        help="Days duration of the status.")
+    time_diff_hour = fields.Char(   
+        string="Time Diff (hours)",
+        help="Hours duration of the status.")
+    time_diff_min = fields.Char(    
+        string="Time Diff (minutes)",
+        help="Minutes duration of the status.")
+    time_diff_sec = fields.Char(    
+        string="Time Diff (seconds)",
+        help="Seconds duration of the status.")
 
 class FamilyMemberHistoryReasonReference(models.Model):  
     _name = "hc.family.member.history.reason.reference"   
