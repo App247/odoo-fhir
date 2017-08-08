1bc3b263cbe57acfba107219edc4374188b61b1e
Luigi Sison <lsison@moxylus.com>
2017-08-07 21:40:32 -0700

    changes

--- addons/hc_structure_definition/__openerp__.py
+++ addons/hc_structure_definition/__openerp__.py
@@ -34,7 +34,7 @@
     # always loaded
     'data': [
         'security/ir.model.access.csv',
-        # 'data/data_structure_definition_ccda_care_plan.xml',
+        'data/data_structure_definition_ccda_care_plan.xml',
         'data/data_structure_definition_us_core_care_plan.xml',
         'views/hc_res_structure_definition_views.xml',
         'views/hc_res_structure_definition_templates.xml',
